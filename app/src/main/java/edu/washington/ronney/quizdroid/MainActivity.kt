44b556fb036f3317e88afd95e6b4014e39e1cb89
ronney221 <ronneydo1@gmail.com>
2018-04-23 21:49:50 -0700

    fixed to list view

--- app/src/main/java/edu/washington/ronney/quizdroid/MainActivity.kt
+++ app/src/main/java/edu/washington/ronney/quizdroid/MainActivity.kt
@@ -3,31 +3,50 @@ package edu.washington.ronney.quizdroid
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import edu.washington.ronney.quizdroid.R.id.listView
+import android.widget.TextView
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    val names = arrayOf("Math", "Physics", "Marvel Super Heroes")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mathButton = findViewById<View>(R.id.button)
-        val pButton = findViewById<View>(R.id.button2)
-        val mButton = findViewById<View>(R.id.button3)
+        val listView = findViewById<ListView>(R.id.listView)
 
-        mathButton.setOnClickListener {
-            val intent = Intent(this, MathOverviewActivity::class.java)
-            startActivity(intent)
+        val adapter = ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, names)
+
+        listView.adapter = adapter
+
+
+
+        val clickListener = AdapterView.OnItemClickListener { parent, v, position, id ->
+            if (names[position] == "Math") {
+                val intent = Intent(this, MathOverviewActivity::class.java)
+                startActivity(intent)
+            } else if (names[position] == "Physics") {
+                val intent = Intent(this, PhysicsOverviewActivity::class.java)
+                startActivity(intent)
+            } else if (names[position] == "Marvel Super Heroes") {
+                val intent = Intent(this, MarvelOverviewActivity::class.java)
+                startActivity(intent)
+            }
+
+            Log.i("MainActivity", "You selected ${names[position]}")
         }
 
-        pButton.setOnClickListener {
-            val intent = Intent(this, PhysicsOverviewActivity::class.java)
-            startActivity(intent)
-        }
+        listView.setOnItemClickListener(clickListener)
 
-        mButton.setOnClickListener {
-            val intent = Intent(this, MarvelOverviewActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
